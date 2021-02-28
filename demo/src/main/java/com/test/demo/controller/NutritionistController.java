2b2edb2587e3167d089ddaf72347a5a94ddfbfc7
Ronaldop <datascience819@gmail.com>
2021-02-28 17:59:29 -0300

    API pronta para test com o banco de dados e o postman

--- demo/src/main/java/com/test/demo/controller/NutritionistController.java
+++ demo/src/main/java/com/test/demo/controller/NutritionistController.java
@@ -1,13 +1,25 @@
 package com.test.demo.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.test.demo.datasource.model.Nutritionist;
+import com.test.demo.repository.NutritionistRepository;
 
 @RestController
 public class NutritionistController {
 
+	@Autowired
+	private NutritionistRepository nutritionistRepository;
+	
 	@GetMapping(path = "api/nutricionista-id/{}")
-	public void searchNutritionistForId() {
+	public ResponseEntity<Optional<Nutritionist>> searchNutritionistForId(@PathVariable (name = "id", required = true)Long idNutritionist) {
+		return ResponseEntity.ok(nutritionistRepository.findById(idNutritionist));
 		
 	}
 	
